bbfacf709986aa4bb4d5fb017a3e3922d817fa99
Andy Campbell <andycampbelldev@gmail.com>
2022-04-26 13:23:37 -0400

    start work on Navbar component and tests

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Navbar() {
+    return (
+        <nav>Reddit-Client</nav>
+    )
+}
+
+export default Navbar;
---
+++ src/components/Navbar/Navbar.test.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { screen, render } from '@testing-library/react';
+import Navbar from './Navbar'
+//import { Provider } from 'react-redux';
+//import { store } from './app/store';
+//import App from './App';
+
+describe('Navbar component testing', () => {
+    test('Navbar will render and contains the name "Reddit-Client"', () => {
+        render(<Navbar />);
+        // Navbar has rendered and is identifiable by navigation role
+        const navbar = screen.getByRole('navigation');
+        expect(navbar).toBeInTheDocument();
+        // "Reddit Client" is on the page
+        const appName = screen.getByText('Reddit-Client');
+        expect(appName).toBeInTheDocument();
+    })
+})
