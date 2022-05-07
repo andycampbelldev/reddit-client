827fb1f3bb1b458b42c4facb3a7cfb76f499eb8a
Andy Campbell <andycampbelldev@gmail.com>
2022-05-07 10:43:10 -0700

    move PostCard background styles into css

--- src/components/PostCard/PostCard.js
+++ src/components/PostCard/PostCard.js
@@ -10,7 +10,7 @@ export default function PostCard(props) {
     const background = hasImage ? `linear-gradient(180deg, #000000cc -20%, transparent), url(${data.url})` : `linear-gradient(0deg, rgba(2,0,36,1) 0%, rgba(12,154,201,0.16152398459383754) 0%, rgba(0,212,255,0.16)`
     return (
         <Col sm={{size: 6}} md={{size: 4}} className='d-flex align-items-stretch mb-2 px-1'>
-            <Card className='PostCard flex-grow-1' style={{backgroundImage: background, backgroundSize: 'cover', backgroundPosition: 'center'}}>
+            <Card className='PostCard flex-grow-1' style={{backgroundImage: background}}>
                 <CardBody>
                     <CardTitle tag='h3' className={hasImage && 'text-light'}>
                         {title.length > 100 ? `${title.substring(0, 99)}...` : title}
