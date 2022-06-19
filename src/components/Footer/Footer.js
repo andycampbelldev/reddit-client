01f01de3ca64594a680d4dcef6d7d40f66f8c3f1
Andy Campbell <andycampbelldev@gmail.com>
2022-06-18 17:14:09 -0700

    add noreferrer attribute to link to portfolio site

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ import './Footer.css'
 export default function Footer(props) {
     return (
         <footer className='Footer p-3 text-muted'>
-            Built with React and Redux by Andy Campbell. <a href='https://andycampbell.io' target='_blank'>www.andycampbell.io</a>
+            Built with React and Redux by Andy Campbell. <a href='https://andycampbell.io' noreferrer target='_blank'>www.andycampbell.io</a>
         </footer>
     )
 }
