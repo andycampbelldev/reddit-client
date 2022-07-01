b7f0c20df2b86f04da09ed17f6bbaab0e0aad1a7
Andy Campbell <andycampbelldev@gmail.com>
2022-07-01 15:07:10 -0700

    add Theme slice to support light or dark mode

--- src/app/store.js
+++ src/app/store.js
@@ -3,9 +3,11 @@ import searchTermReducer from '../features/SearchForm/SearchTermSlice';
 import subredditReducer from '../features/SubredditNav/SubredditSlice';
 import postsReducer from '../features/PostsGrid/PostsSlice';
 import postReducer from '../features/Post/PostSlice';
+import themeReducer from '../features/Theme/ThemeSlice';
 
 export const store = configureStore({
   reducer: {
+    theme: themeReducer,
     searchTerm: searchTermReducer,
     subreddits: subredditReducer,
     posts: postsReducer,
---
+++ src/features/Theme/ThemeSlice.js
@@ -0,0 +1,27 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const options = {
+    name: 'theme',
+    initialState: {
+        darkMode: false 
+    },
+    reducers: {
+        setDarkMode: (state) => {
+            const savedTheme = window.localStorage.getItem("redditBoardTheme");
+            if(!savedTheme) return;
+            let savedDarkMode = JSON.parse(savedTheme).darkMode;
+            if (!savedDarkMode) return;
+            state.darkMode = savedDarkMode;
+        },
+        toggleDarkMode: (state) => {
+            state.darkMode = !state.darkMode;
+            window.localStorage.setItem("redditBoardTheme", JSON.stringify({ darkMode: state.darkMode }));
+        }
+    }
+}
+
+const themeSlice = createSlice(options);
+
+export const selectDarkMode = state => state.theme.darkMode;
+export const { setDarkMode, toggleDarkMode } = themeSlice.actions;
+export default themeSlice.reducer;
