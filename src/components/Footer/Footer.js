44b412efe1c1f19d7d6572c3cfd39712b7923922
Andy Campbell <andycampbelldev@gmail.com>
2022-07-24 16:07:07 -0700

    improve accessibility of footer text

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -4,7 +4,7 @@ import './Footer.css'
 
 export default function Footer(props) {
     return (
-        <footer className='Footer p-3 text-muted'>
+        <footer className='Footer p-3'>
             Built with React and Redux by Andy Campbell. <a href='https://andycampbell.io' rel='noreferrer' target='_blank'>www.andycampbell.io</a>
         </footer>
     )
