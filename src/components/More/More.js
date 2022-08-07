c887b9cd1ab88f46d8dfc1a5120fb1bbfe1fcb49
Andy Campbell <andycampbelldev@gmail.com>
2022-08-06 22:12:41 -0700

    standardize styling on More component

--- src/components/More/More.js
+++ src/components/More/More.js
@@ -9,8 +9,8 @@ export default function More({ moreWhat, onClick }) {
 
     return (
         <div className='More d-flex my-2'>
-            <div className='More-blockquote'></div>
-            <button onClick={handleClick} className='More-button'>more {moreWhat}</button> 
+            <div className='More-blockquote me-2'></div>
+            <button onClick={handleClick} className='More-button d-block border-0 py-2'>more {moreWhat}</button> 
         </div>
     )
 }
